f24817a61207ff490647bf1eaf40731491e2e15c
BarreraJoel <joelmanuelbar@gmail.com>
2025-07-22 01:09:45 -0300

    eliminar carpeta dist

--- dist/config/app.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = void 0;
-require('dotenv').config();
-exports.app = {
-    port: (_a = process.env.APP_PORT) !== null && _a !== void 0 ? _a : 3000,
-};
--- dist/config/jwt.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jwt = void 0;
-require('dotenv').config();
-exports.jwt = {
-    secret: (_a = process.env.JWT_SECRET) !== null && _a !== void 0 ? _a : "",
-};
